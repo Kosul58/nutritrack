a7c3fbdec3ad441383b30cc75280854b6a01f9bc
Kosul58 <kosulgrg@gmail.com>
2024-06-21 22:53:08 +0545

    q

--- nutriapp/src/Components/3About.js
+++ nutriapp/src/Components/3About.js
@@ -30,17 +30,19 @@ const About = () => {
         </div>
       </div>
       <div className="about-section-text-container">
-        <h1 className="primary-heading">
-          Food Is An Important Part Of A Balanced Diet
-        </h1>
-        <p className="primary-text">
-          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
-          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
-        </p>
-        <p className="primary-text">
-          Non tincidunt magna non et elit. Dolor turpis molestie dui magnis
-          facilisis at fringilla quam.
-        </p>
+        <div className="a1">
+          <h1 className="primary-heading">
+            Food Is An Important Part Of A Balanced Diet
+          </h1>
+          <p className="primary-text">
+            Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+            elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+          </p>
+          <p className="primary-text">
+            Non tincidunt magna non et elit. Dolor turpis molestie dui magnis
+            facilisis at fringilla quam.
+          </p>
+        </div>
       </div>
     </div>
   );
