4c2d37d322b7ff13df4c6fb2d5c9acd4663a46f8
Kosul58 <kosulgrg@gmail.com>
2024-06-22 18:11:38 +0545

    a

--- nutriapp/src/Components/44.js
+++ nutriapp/src/Components/44.js
@@ -1,5 +1,12 @@
 import React, { useEffect } from "react";
 import { useState, useRef } from "react";
+
+import AboutBackground from "../Assets/about-background.png";
+import img1 from "../Assets/fc11.png";
+import img2 from "../Assets/workk.jpg";
+import img3 from "../Assets/fc3.png";
+import img4 from "../Assets/fc4.png";
+
 import img5 from "../Assets/infox11.png";
 import img6 from "../Assets/infox22.png";
 
@@ -7,21 +14,58 @@ import "./44.css";
 
 const Work = () => {
   return (
-    <div className="work-section-wrapper slide">
-      <div className="blackscarf"></div>
-      <div className="infox">
-        <h1 className=" b113">You Are </h1>
-        <div className="infoximg">
-          <img src={img5} alt="" />
+    <>
+      <div className="about-section-container slide">
+        <div className="about-background-image-container">
+          <img src={AboutBackground} alt="" />
+        </div>
+        <div className="about-section-image-container">
+          <div className="k1">
+            <div className="k11 block">
+              <img src={img3} alt="" className="img3" />
+            </div>
+            <div className="k12 block">
+              <img src={img1} alt="" className="img1" />
+            </div>
+          </div>
+          <div className="k1">
+            <div className="k12 block">
+              <img src={img2} alt="" className="img2" />
+            </div>
+            <div className="k11 block">
+              <img src={img4} alt="" className="img4" />
+            </div>
+          </div>
+        </div>
+        <div className="about-section-text-container block">
+          <div className="a1">
+            <h1 className="about-heading animate-pop-in">
+              Food Is An Important Part Of A Balanced Diet
+            </h1>
+            <p className="about-text animate-pop-in">
+              Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+              elit. Dolor turpis molestie dui magnis facilisis at fringilla
+              quam.
+            </p>
+          </div>
         </div>
       </div>
-      <div className="infox infox2">
-        <div className="infoximg">
-          <img src={img6} alt="" />
+      <div className="work-section-wrapper slide">
+        <div className="blackscarf"></div>
+        <div className="infox">
+          <h1 className=" b113">You Are </h1>
+          <div className="infoximg">
+            <img src={img5} alt="" />
+          </div>
         </div>
-        <h1 className=" b113">What you eat </h1>
+        <div className="infox infox2">
+          <div className="infoximg">
+            <img src={img6} alt="" />
+          </div>
+          <h1 className=" b113">What you eat </h1>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
