849b87fbcdf07ea5f9cd895876b5cbbc74d1a1ac
Kosul58 <kosulgrg@gmail.com>
2024-06-26 13:48:09 +0545

    a

--- nutriapp/src/Components/66.js
+++ nutriapp/src/Components/66.js
@@ -12,19 +12,102 @@ import img8 from "../Assets/control-img-4.png";
 
 import "./66.css";
 
-const aii = () => {
+const Aii = () => {
   const circleref = useRef(null);
 
-  let i = 0,
-    j = 1,
-    intervalid;
+  const rectangleref1 = useRef(null);
+  const rectangleref2 = useRef(null);
+  const rectangleref3 = useRef(null);
+  const rectangleref4 = useRef(null);
+
+  const rectangleref = [
+    rectangleref1,
+    rectangleref2,
+    rectangleref3,
+    rectangleref4,
+  ];
+
+  const controlref1 = useRef(null);
+  const controlref2 = useRef(null);
+  const controlref3 = useRef(null);
+  const controlref4 = useRef(null);
+  const controlref = [controlref1, controlref2, controlref3, controlref4];
 
   const intervalfn = () => {
+    let i = 0,
+      j = 1,
+      k = 1,
+      intervalid;
     intervalid = setInterval(() => {
-      circleref.current.style.rotate = `-${++i * 90}deg`;
-      console.log(i);
-    }, 4000);
+      if (circleref.current) {
+        circleref.current.style.rotate = `-${++i * 90}deg`;
+      }
+    }, 3000);
   };
+
+  intervalfn();
+
+  const intervalfn2 = (a) => {
+    let i = 0,
+      j = 1,
+      intervalid;
+    intervalid = setInterval(() => {
+      let k;
+      if (i == 0) {
+        k = 3;
+      } else if (i == 1) {
+        k = 0;
+      } else if (i == 2) {
+        k = 1;
+      } else if (i == 3) {
+        k = 2;
+      }
+
+      if (a[i].current) {
+        if (a[k].current) {
+          a[k].current.style.opacity = `0`;
+          a[i].current.style.transform = `translateX(-40px)`;
+          a[i].current.style.opacity = `1`;
+        }
+      }
+
+      i++;
+      i == 4 ? (i = 0) : (i = i);
+    }, 3000);
+  };
+
+  intervalfn2(rectangleref);
+
+  const intervalfn3 = (a) => {
+    let i = 1,
+      j = 1,
+      intervalid;
+    intervalid = setInterval(() => {
+      let k;
+      if (i == 0) {
+        k = 3;
+      } else if (i == 1) {
+        k = 0;
+      } else if (i == 2) {
+        k = 1;
+      } else if (i == 3) {
+        k = 2;
+      }
+
+      if (a[i].current) {
+        if (a[k].current) {
+          a[k].current.classList.remove("active");
+          a[i].current.classList.add("active");
+        }
+      }
+
+      i++;
+      i == 4 ? (i = 0) : (i = i);
+    }, 3000);
+  };
+
+  intervalfn3(controlref);
+
   return (
     <>
       <div className="containerx">
@@ -43,10 +126,37 @@ const aii = () => {
               <img src={img4} alt="" />
             </div>
           </div>
+          <div className="carousely">
+            <div ref={controlref1} className="asss">
+              <img src={img5} alt="" />
+            </div>
+
+            <div ref={controlref2} className="asss">
+              <img src={img6} alt="" />
+            </div>
+
+            <div ref={controlref3} className="asss">
+              <img src={img8} alt="" />
+            </div>
+
+            <div ref={controlref4} className="asss">
+              <img src={img7} alt="" />
+            </div>
+          </div>
+          <div ref={rectangleref1} className="carouselz z1"></div>
+          <div ref={rectangleref2} className="carouselz z2">
+            {" "}
+          </div>
+          <div ref={rectangleref3} className="carouselz z3">
+            {" "}
+          </div>
+          <div ref={rectangleref4} className="carouselz z4">
+            {" "}
+          </div>
         </div>
       </div>
     </>
   );
 };
 
-export default aii;
+export default Aii;
